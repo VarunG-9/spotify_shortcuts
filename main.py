600302f0af882cf8b5c3477036721205049a3924
Varun G <varungadde@outlook.com>
2024-05-01 12:05:17 +0530

    Working - there is still a bug which makes it not work on startup

--- main.py
+++ main.py
@@ -1,101 +1,66 @@
+import sys
 import os
 import spotipy
 import keyboard
-from spotipy.oauth2 import SpotifyOAuth
-from time import sleep
 import pickle
+from time import sleep
+from spotipy.oauth2 import SpotifyOAuth
+
+# Redirect stdout and stderr to log files
+stdout_path = r'C:\Users\aweclops\Documents\Python\Spotify_Shortcuts\stdout.log'
+stderr_path = r'C:\Users\aweclops\Documents\Python\Spotify_Shortcuts\stderr.log'
+
+try:
+    sys.stdout = open(stdout_path, 'w')
+    sys.stderr = open(stderr_path, 'w')
+except Exception as e:
+    print(f"Error: {e}")
+
+print("Finished redirecting stdout and stderr.")
 
 current_vol = 69
+
 def save_vol():
     global current_vol
     with open(r'C:\Users\aweclops\AppData\Roaming\Spotify\volume.pickle', 'wb') as file:
         pickle.dump(current_vol, file)
 
-def resume():
-    sp.start_playback(device_id=None, context_uri=None, uris=None, offset=None, position_ms=None)
+# Define other functions...
 
-def pause():
-    sp.pause_playback()
+print("Finished defining variables.")
 
-def invert_playback():
-    if sp.current_user_playing_track()['is_playing']:
-        pause()
-    else: 
-        resume()
-
-def skip():
-    sp.next_track(device_id=None)
-
-def go_back():
-    sp.previous_track(device_id=None)
-
-def play_playlist_1():
-    sp.start_playback(device_id=None, context_uri='spotify:playlist:09ArxQD7XoZKgXBgYvHuDp', uris=None, offset=None, position_ms=None)
-
-def play_playlist_2():
-    sp.start_playback(device_id=None, context_uri='spotify:playlist:4U3OIyfLyXfAsEVTcFhMWK', uris=None, offset=None, position_ms=None)
-
-def vol_up():
-    print('vol up')
-    global current_vol
-    current_vol+=5
-    if current_vol > 100:
-        current_vol=100
-    sp.volume(current_vol)
-    save_vol()
-
-def vol_down():
-    print('vol down')
-    global current_vol
-    current_vol-=5
-    if current_vol<0:
-        current_vol=0
-    sp.volume(current_vol)
-    save_vol()
-
-
-# Checking if volume file exists, otherwise create.
+# Check and create volume file if not exists
 path = r'C:\Users\aweclops\AppData\Roaming\Spotify\volume.pickle'
-if os.path.isfile(path):
-    pass
-else: 
-    f = open(path, "x")
-    save_vol()
-    
-# Loading Volume
+if not os.path.isfile(path):
+    try:
+        with open(path, "x") as f:
+            save_vol()
+    except Exception as e:
+        print(f"Error: {e}")
 
+print("Checked whether volume file exists.")
+
+# Loading Volume
 try:    
     with open(r'C:\Users\aweclops\AppData\Roaming\Spotify\volume.pickle', 'rb') as file:
         current_vol=pickle.load(file)
-        print("loaded volume")
-except: 
-    print('error: not loaded volume')
-
+        print("Loaded volume")
+except Exception as e:
+    print(f"Error: {e}")
 
 scope = "user-read-playback-state,user-modify-playback-state"
 sp = spotipy.Spotify(client_credentials_manager=SpotifyOAuth(scope=scope))
 
-
-print('opening spotify')
-os.startfile(r'C:\Users\aweclops\AppData\Roaming\Spotify\Spotify.exe')
-print('opened spotify')
-
 while True:
     try:
         sp.volume(current_vol)
-        print('connecting')
+        print('Connected')
         break
-        
-    except:
-        print('failed to connect. trying again')
-        
+    except Exception as e:
+        print(f"Failed to connect: {e}. Trying again.")
+        sleep(10)
 
-keyboard.add_hotkey('print screen', lambda: invert_playback())
-keyboard.add_hotkey('ctrl+page up',lambda: play_playlist_1())
-keyboard.add_hotkey('ctrl+page down',lambda: play_playlist_2())
-keyboard.add_hotkey('ctrl+right', lambda: skip())
-keyboard.add_hotkey('ctrl+left', lambda: go_back())
-keyboard.add_hotkey('ctrl+up', lambda: vol_up())
-keyboard.add_hotkey('ctrl+down', lambda: vol_down())
+# Define hotkeys...
 
-keyboard.wait('esc')
+keyboard.wait()
+print("Added keyboard hotkeys.")
