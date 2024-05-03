b16dc6490036849cf2276fe6ae5b3e552eeab92d
Varun G <varungadde@outlook.com>
2024-05-03 13:01:55 +0530

    Fixed error when spotify is closed after the program being used for a bit.
    Added logs to stderr and stdout.

--- main.py
+++ main.py
@@ -25,44 +25,58 @@ def save_vol():
         pickle.dump(current_vol, file)
 
 def resume():
+    wait_for_connection()
     sp.start_playback(device_id=None, context_uri=None, uris=None, offset=None, position_ms=None)
 
 def pause():
+    wait_for_connection()
     sp.pause_playback()
 
 def invert_playback():
-    if sp.current_user_playing_track()['is_playing']:
-        pause()
-    else: 
-        resume()
+    wait_for_connection()
+    try:
+        if sp.current_user_playing_track()['is_playing']:
+            pause()
+        else: 
+            resume()
+    except:
+        print("Lost connection to device")
+        check_connection()
+
 
 def skip():
+    wait_for_connection()
     sp.next_track(device_id=None)
 
 def go_back():
+    wait_for_connection()
     sp.previous_track(device_id=None)
 
 def play_playlist_1():
+    wait_for_connection()
     sp.start_playback(device_id=None, context_uri='spotify:playlist:09ArxQD7XoZKgXBgYvHuDp', uris=None, offset=None, position_ms=None)
 
 def play_playlist_2():
+    wait_for_connection()
     sp.start_playback(device_id=None, context_uri='spotify:playlist:4U3OIyfLyXfAsEVTcFhMWK', uris=None, offset=None, position_ms=None)
 
 def vol_up():
-    print('vol up')
+    wait_for_connection()
     global current_vol
     current_vol+=5
     if current_vol > 100:
         current_vol=100
     sp.volume(current_vol)
+    print(f'vol up. new vol: {current_vol}')
     save_vol()
 
 def vol_down():
-    print('vol down')
+    wait_for_connection()
     global current_vol
     current_vol-=5
     if current_vol<0:
         current_vol=0
+    print(f'vol down. new vol: {current_vol}')
     sp.volume(current_vol)
     save_vol()
 
@@ -81,23 +95,32 @@ print("checked whether volume file exists.")
 try:    
     with open(r'C:\Users\aweclops\AppData\Roaming\Spotify\volume.pickle', 'rb') as file:
         current_vol=pickle.load(file)
-        print("loaded volume")
+        print(f"loaded volume. current volume {current_vol}")
 except: 
     print('error: not loaded volume')
 
 scope = "user-read-playback-state,user-modify-playback-state"
 sp = spotipy.Spotify(client_credentials_manager=SpotifyOAuth(scope=scope))
 
-while True:
+def is_connected():
     try:
         sp.volume(current_vol)
-        print('connected')
-        break
-        
+        print("Connected to spotify.")
+        return True
+
     except:
-        print('failed to connect. trying again')
-        sleep(10)
-        
+            print('Failed to connect to spotify.')
+            return False
+
+def wait_for_connection():
+    while True:
+        if is_connected():
+            break
+        else:
+            sleep(7)
+
+
+
 keyboard.add_hotkey('print screen', lambda: invert_playback())
 keyboard.add_hotkey('ctrl+page up',lambda: play_playlist_1())
 keyboard.add_hotkey('ctrl+page down',lambda: play_playlist_2())
@@ -105,5 +128,5 @@ keyboard.add_hotkey('ctrl+right', lambda: skip())
 keyboard.add_hotkey('ctrl+left', lambda: go_back())
 keyboard.add_hotkey('ctrl+up', lambda: vol_up())
 keyboard.add_hotkey('ctrl+down', lambda: vol_down())
-keyboard.wait()
 print("added keyboard hotkeys.")
+keyboard.wait()
